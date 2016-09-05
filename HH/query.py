6942287a2a69cbc0196dcd71ecffb18b40485a64
Vladislav Pereskokov <vladpereskokov@mail.ru>
2016-09-05 21:32:49 +0300

    Update query.py

--- HH/query.py
+++ HH/query.py
@@ -10,8 +10,7 @@ from bs4 import BeautifulSoup
 import time
 # Для записи данных в файл с расширением 'csv'
 import csv
-# Обработка валют
-import currency_parser as cur_pars
+import threading
 # 'Синонимы'
 import defines
 
@@ -33,7 +32,7 @@ def switch_y_n(case):
         'y': True,
         'n': False
     }
-    return switcher.get(case, False)
+    return switcher.get(case, False)    
 
 
 # HTTP - запрос
@@ -119,8 +118,7 @@ def find_languages(description):
     languages = ''
     for language in defines.LANGUAGES:
         if description.upper().find(language) is not -1:
-            if not check_languages(language, languages):
-                languages += language + ' . '
+            languages += language + ' . '
     return languages[:len(languages) - 2]
 
 
@@ -135,10 +133,15 @@ def research_job(page, _date):
         price = price[1:]
     location = main_info.find('td', class_='l-content-colum-2 b-v-info-content').text
     experience = main_info.find('td', class_='l-content-colum-3 b-v-info-content').text
+    isadd = True
+    date = ''
+    languages = ''
     if all_items.find('table', class_='l-content-2colums b-vacancy-container') is not None:
         description = all_items.find('table', class_='l-content-2colums b-vacancy-container') \
             .find('div', class_='b-vacancy-desc-wrapper').text
         languages = find_languages(description)
+        if len(languages) == 0:
+            isadd = False
         date = all_items.find('table', class_='l-content-2colums b-vacancy-container') \
             .find('td', class_='l-content-colum-2').find('time', class_='vacancy-sidebar__publication-date').text
         date = string_to_date(date)
@@ -146,12 +149,13 @@ def research_job(page, _date):
         # par1 - Продолжать ли просмотр других вакансий
         # par2 - Добавлять ли эту работу в общий список
         # par3 - Работа
-        return [True, False, 0]
+        isadd = False
     is_continue = True
     print(date, _date)
     if date == _date:
         is_continue = False
-    return [is_continue, True, {
+    print('\n\n')
+    return [is_continue, isadd, {
         defines.TITLE: title,
         defines.PRICE: price,
         defines.SKILL: languages,
@@ -165,7 +169,7 @@ def research_job(page, _date):
 def query_handle(html, jobs, _date, _class):
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('table', class_='l l_auto')
-    all_items = table.find('div', class_='search-result') \
+    all_items = table.find('div', class_='search-result')\
         .find_all('div', class_=_class)
     for i in range(len(all_items)):
         id = all_items[i].select('a')
@@ -195,30 +199,44 @@ def save_jobs(jobs, path):
              str(job[defines.DATE])[:11]) for job in jobs)
 
 
+def parse_thread(date, jobs, url, _class):
+    i = 0
+    while parse(date, jobs, i, url,
+                _class):
+        i += 1
+        print('page: ', i)
+        if i == 99:
+            break
+
+
 # Обработка сайта hh.ru
 def parsing(days, path, sort=True):
     jobs = []
     print('Обработка данных...\nПожалуйста подождите...')
     date = search_days(days)
     print(date)
-    i = 0
-    while parse(date, jobs, i, defines.URL, defines.PREMIUM_CLASS):
-        i += 1
-        print('prem')
-        if i == 99:
-            break
-    i = 0
-    while parse(date, jobs, i, defines.URL, defines.STANDARD_CLASS):
-        i += 1
-        print('stand')
-        if i == 99:
-            break
-    i = 0
-    while parse(date, jobs, i, defines.URL, defines.STANDARD_PLUS_CLASS):
-        i += 1
-        print('stand_plus')
-        if i == 99:
-            break
+    thread1 = threading.Thread(target=parse_thread, name='name1', args=[date, jobs, defines.URL, defines.PREMIUM_CLASS])
+    thread2 = threading.Thread(target=parse_thread, name='name2', args=[date, jobs, defines.URL, defines.STANDARD_CLASS])
+    thread3 = threading.Thread(target=parse_thread, name='name3', args=[date, jobs, defines.URL, defines.STANDARD_PLUS_CLASS])
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread1.join()
+    thread2.join()
+    thread3.join()
+
+    # i = 0
+    # while parse(date, jobs, i, defines.URL, ):
+    #     i += 1
+    #     print('stand: ', i)
+    #     if i == 99:
+    #         break
+    # i = 0
+    # while parse(date, jobs, i, defines.URL, ):
+    #     i += 1
+    #     print('stand_plus: ', i)
+    #     if i == 99:
+    #         break
     jobs.sort(key=lambda x: x[defines.DATE], reverse=sort)
     print('Сохранени данных...')
     save_jobs(jobs, path)
